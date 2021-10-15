998de4a9defcfb3755f3b8fdfdec41b6b4b2f7be
rakshithr0177 <85289318+rakshithr0177@users.noreply.github.com>
2021-10-15 17:46:05 +0530

    Update problem1.c

--- reftest/problem1.c
+++ reftest/problem1.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main()
+{
+    float base, altitude, area;
+    printf("Enter base and alititude\n");
+    scanf("%f%f", &base, &altitude);
+    area = 0.5 * base * altitude;
+    printf("Area of the triange with base %f and altitude %f is %f\n", base, altitude, area);
+    return 0;
+}
